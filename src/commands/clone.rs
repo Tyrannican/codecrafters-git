16fb7f026d6241dd2aef19096e277ac615297496
Graham Keenan <graham.keenan@outlook.com>
2024-04-09 19:58:05 +0100

    got git pack request done

--- src/commands/clone.rs
+++ src/commands/clone.rs
@@ -1,36 +1,40 @@
 use anyhow::{Context, Result};
-use reqwest::blocking::{Client, Response};
+use reqwest::blocking::Client;
 use reqwest::StatusCode;
+use std::collections::HashSet;
 
 pub(crate) fn invoke(url: String, _dst: String) -> Result<()> {
     let client = Client::new();
-    let _refs = ref_discovery(&url, &client).context("ref discovery")?;
-    // negotiation(&url, &client, refs).context("negotiation part")?;
-
-    // TODO: Parse refs into PKT types
+    let advertised = ref_discovery(&url, &client).context("ref discovery")?;
+    negotiation(&url, advertised, &client)?;
 
     Ok(())
 }
 
-fn _negotiation(url: &str, client: &Client, refs: Vec<String>) -> Result<()> {
+fn negotiation(url: &str, advertised: Vec<String>, client: &Client) -> Result<()> {
+    let mut common: HashSet<String> = HashSet::default();
+    let want: HashSet<String> = advertised.into_iter().collect();
+
     let url = format!("{url}/git-upload-pack");
-    let test = "0a53e9ddeaddad63ad106860237bbf53411d11a7";
-    println!("Length: {}", test.len());
-    let mut data = Vec::new();
-    data.extend(b"0049want ");
-    data.extend(refs[0].as_bytes());
-    data.push(0x0a);
-    data.extend(b"0000");
+    let mut data = vec![];
+    for r in want.iter() {
+        let content = format!("{}want {}\n", hex::encode(50_u16.to_be_bytes()), r);
+        data.extend(content.bytes());
+    }
+    data.extend(b"00000009done\n");
+
+    println!("{}", String::from_utf8(data.clone())?);
 
     let response = client
-        .post(&url)
-        .header("Content-Type", "application/x-git-upload-pack-request")
-        .header("Content-Length", data.len())
+        .post(url)
+        .header("Content-Type", "x-git-upload-pack-request")
         .body(data)
         .send()
-        .context("git upload pack request")?;
+        .context("sending the git upload pack request")?
+        .text()
+        .context("converting to human readable")?;
 
-    println!("response: {response:?}");
+    //println!("Response: {response:?}");
 
     Ok(())
 }
@@ -44,40 +48,54 @@ fn ref_discovery(url: &str, client: &Client) -> Result<Vec<String>> {
         .send()
         .context("initiating ref discovery")?;
 
-    let raw_refs = validate_ref_discovery(response).context("ref discovery validation")?;
+    let status = response.status();
+    let response = response
+        .text()
+        .context("converting ref discovery response to bytes")?;
 
-    Ok(vec![])
+    println!("Response: {response}");
+    let refs: Vec<_> = response.split('\n').map(|r| r.to_string()).collect();
+    validate_ref_header(&refs[0], status).context("validating ref discovery header")?;
+
+    // This is dirty
+    let mut discovered = Vec::new();
+    let refs = &refs[1..];
+    let head = &refs[0];
+    let Some((head, _)) = head[8..].split_once(' ') else {
+        anyhow::bail!("this is not a reference");
+    };
+
+    discovered.push(head.to_owned());
+    for reference in refs[1..].into_iter() {
+        if reference == "0000" {
+            break;
+        }
+
+        let Some((reference, _)) = reference[4..].split_once(' ') else {
+            anyhow::bail!("this is not a reference");
+        };
+        discovered.push(reference.to_owned());
+    }
+
+    Ok(discovered)
 }
 
 // TODO: Maybe Byte parse or somethign
 // Each ref is preceded by 4 bytes detailing the length
 // e.g. 003e | <some_hash>
 // That's why it never worked...
-fn validate_ref_discovery(response: Response) -> Result<Vec<String>> {
-    match response.status() {
+fn validate_ref_header(header: &str, status: StatusCode) -> Result<()> {
+    match status {
         StatusCode::OK | StatusCode::NOT_MODIFIED => {}
-        _ => anyhow::bail!(format!(
-            "failed status code validation: {}",
-            response.status()
-        )),
+        _ => anyhow::bail!(format!("failed status code validation: {}", status)),
     }
 
     let re = regex::Regex::new(r"^[0-9a-f]{4}# service=git-upload-pack")
         .context("creating validation regex")?;
 
-    let text = response.text().context("converting response to text")?;
-    if !re.is_match(&text) {
+    if !re.is_match(&header) {
         anyhow::bail!("failed regex validation");
     }
 
-    if !text.ends_with("0000") {
-        anyhow::bail!("missing pkt-line marker 0000");
-    }
-
-    let refs = text
-        .split('\n')
-        .map(|r| r.to_owned())
-        .collect::<Vec<String>>();
-
-    Ok(refs)
+    Ok(())
 }
